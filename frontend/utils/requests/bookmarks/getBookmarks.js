084e1e53d9549fe82307df2f5946239600d94fa9
rajshree0412 <rajshreepimple4@gmail.com>
2026-04-18 23:36:05 +0530

    Add getBookmarks function to fetch bookmarks

---
+++ frontend/utils/requests/bookmarks/getBookmarks.js
@@ -0,0 +1,10 @@
+export const getBookmarks = async (setBookmarks)=>{
+
+    const response = await fetch("/bookmarks");
+    // console.log("response: ",response);
+    const data = await response.json();
+    // console.log("data: ",data);
+    setBookmarks(data);
+    return data;
+
+} 
